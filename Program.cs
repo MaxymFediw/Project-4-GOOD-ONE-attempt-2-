0bec447a547c4239282e22e02c26c64f25decf03
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-12 12:17:21 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Project_4__GOOD_ONE_attempt_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Maxym F.
+        }
+        public static void Part1()
+        {
+            string name;
+
+
+
+        }
+        
+    }
+}

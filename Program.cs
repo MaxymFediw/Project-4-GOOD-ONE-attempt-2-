ca92a9ef40ffba7f394c7bdf3b4d4ab7379b6cc2
Maxym Fediw <362706764@GoTVDSB.ca>
2026-02-13 13:21:27 -0500

    Checkpoint 2

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace Project_4__GOOD_ONE_attempt_2_
         {
             //Maxym F.
             
-            Part2();
+            Part5();
 
         }
         public static void Part1()
@@ -23,12 +23,12 @@ namespace Project_4__GOOD_ONE_attempt_2_
             name = Console.ReadLine();
 
             Console.WriteLine("Hi, " + name +"! How old are you?");
-            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out age));
+            Int32.TryParse(Console.ReadLine(), out age);
 
             Console.WriteLine("So, you're " + age+ ", eh? Heh heh-I remember when you were in diapers! So, how much do you make?");
             Double.TryParse(Console.ReadLine(), out income);
 
-            Console.WriteLine(" $" + income + "! Damn! Maybe you should pick up a few extra shifts, heh heh!");
+            Console.WriteLine(income. ToString("C") + "! Damn! Maybe you should pick up a few extra shifts, heh heh!");
 
 
             
@@ -65,7 +65,7 @@ namespace Project_4__GOOD_ONE_attempt_2_
             Double.TryParse(Console.ReadLine(), out grade);
 
             Console.Write("What is your student ID? ");
-            Console.WriteLine(Int32.TryParse(Console.ReadLine(), out studentID));
+            Int32.TryParse(Console.ReadLine(), out studentID);
 
             Console.Write("What is your average? ");
             Double.TryParse(Console.ReadLine(), out average);
@@ -74,8 +74,8 @@ namespace Project_4__GOOD_ONE_attempt_2_
 
             Console.WriteLine("login:\t\t" + login);
             Console.WriteLine("ID:\t\t" + studentID);
-            Console.WriteLine("Name\t\t" + lastName + firstName);
-            Console.WriteLine("Average\t\t" + average);
+            Console.WriteLine("Name\t\t" + lastName +", " + firstName);
+            Console.WriteLine("Average\t\t" + average + "%");
             Console.WriteLine("Grade\t\t" + grade);
            
 
@@ -86,8 +86,109 @@ namespace Project_4__GOOD_ONE_attempt_2_
 
 
         }
+        public static void Part3() 
+        {
+
+
+            string name;
+            int age, futureAge, pastAge;
+
+            
+
+            
+
+
+            Console.WriteLine("Hello! What is your name?"); 
+            name = Console.ReadLine();
+
+            Console.WriteLine("Hi, " + name + "! How old are you?");
+            Int32.TryParse(Console.ReadLine(), out age);
+
+            futureAge = age + 5;
+            pastAge = age - 5;
+
+            Console.WriteLine("Did you know that in five years you will be " + futureAge + " years old? And five years ago you were " + pastAge + "! Imagine that!"); 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        public static void Part4() 
+        {
+
+
+            double number1, number2, number3, divided;
+
+            Console.WriteLine("Input a number:");
+            Double.TryParse(Console.ReadLine(), out number1);
+
+            Console.WriteLine("Input another number:");
+            Double.TryParse(Console.ReadLine(), out number2);
+
+            Console.WriteLine("Input a final number:");
+            Double.TryParse(Console.ReadLine(), out number3);
+
+            divided = (number1  + number2 + number3) / 2;
+
+            Console.WriteLine("Calculations complete! You have " + divided + ".");
+
+
+
+
+
+        }
+        public static void Part5() 
+        {
+
+            string item1, item2;
+            
+            double cost1, cost2, totalCost, discount20, discountedCost, taxTotal, finalTotal;
+
+            Console.WriteLine("Hello! Give me a name of something thats on your grocery list!");
+            item1 = Console.ReadLine();
+
+            Console.WriteLine("And how much does " + item1 + " Usually cost?");
+            Double.TryParse(Console.ReadLine(), out cost1);
+
+            Console.WriteLine("Give me another item on your list.");
+            item2 = Console.ReadLine();
+
+            Console.WriteLine("And how much does " + item2 + " normally set you back?");
+            Double.TryParse(Console.ReadLine(), out cost2);
+
+            totalCost = cost1 + cost2;
+
+            Console.WriteLine("So those two items will cost you " + totalCost.ToString("C") + ". But luckily for you, you'll get 20% off!");
+
+            discount20 = totalCost * 0.2;
+
+            discountedCost = totalCost - discount20;
+
+            Console.WriteLine("So now, you only have to pay " + discountedCost.ToString("C") + "!");
+
+            taxTotal = discountedCost * 0.13;
+
+            finalTotal = taxTotal + discountedCost;
+
+
+            Console.WriteLine("Oh, yeah! I almost forgot tax haha! Here's your real final price: " + finalTotal.ToString("C") + ". Have a good day now!");
+
+
+
+        }
     }
 }
